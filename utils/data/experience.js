27ab0f8bfaff02570044ef40394f59df90d56f98
wiraphat <132466647+wiraphatys@users.noreply.github.com>
2024-08-01 22:55:04 +0700

    feat: update experience on aug 2024

--- utils/data/experience.js
+++ utils/data/experience.js
@@ -1,14 +1,26 @@
 export const experiences = [
   {
     id: 1,
-    title: 'Software Engineer - Backend',
-    company: "BeWell (Digital Commerce Co., Ltd.)",
-    duration: "(Apr 2024 - Present)"
+    title: 'Software Engineer Intern - Full Stack',
+    company: "Apppi Co., Ltd.",
+    duration: "(Aug 2024 - Present)"
   },
   {
     id: 2,
-    title: "Software Engineer - Fullstack",
-    company: "Freelance",
-    duration: "(Aug 2023 - Apr 2024)"
+    title: 'Project Manager',
+    company: "Student Government of Chulalongkorn University (SGCU)",
+    duration: "(June 2024 - Present)"
+  },
+  {
+    id: 3,
+    title: 'Software Engineer - Full Stack',
+    company: "Engineering Student Committee, Chulalongkorn University (ESC CU)",
+    duration: "(June 2024 - Present)"
+  },
+  {
+    id: 4,
+    title: 'Software Engineer Intern - Back End',
+    company: "Digital Commerce Co., Ltd. (Bewell)",
+    duration: "(Apr 2024 - Jul 2024)"
   }
 ]
